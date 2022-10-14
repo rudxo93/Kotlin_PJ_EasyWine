1933da3f6999b31035a9f2a745c41e13576a660e
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-10-14 23:16:00 +0900

    Info -> Info Tab 구성해주던 RecyclerView -> LinearLayout 변경

--- app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/data/model/InfoItemModel.kt
+++
@@ -1,5 +0,0 @@
-package com.duran.gyoung_tae_93.pj.easywine.data.model
-
-data class InfoItemModel(
-    val title : String
-)
--- app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/fragment/InfoFragment.kt
+++ app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/fragment/InfoFragment.kt
@@ -5,30 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.duran.gyoung_tae_93.pj.easywine.R
-import com.duran.gyoung_tae_93.pj.easywine.data.model.InfoItemModel
 import com.duran.gyoung_tae_93.pj.easywine.databinding.FragmentInfoBinding
-import com.duran.gyoung_tae_93.pj.easywine.ui.adapter.InfoRvAdapter
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class InfoFragment : Fragment() {
 
     private lateinit var binding: FragmentInfoBinding
-
-    private val infoRV by lazy { binding.rvInfo }
-
-    private val titleList = arrayListOf<InfoItemModel>(
-        InfoItemModel("와인 글라스 선택하기"),
-        InfoItemModel("와인 오픈하는 법"),
-        InfoItemModel("샴페인 오픈하는 법"),
-        InfoItemModel("오프너가 없을때"),
-        InfoItemModel("코르크 마개가 부러졌을때"),
-        InfoItemModel("와인 빠르게 칠링하는 법"),
-        InfoItemModel("와인 맛있게 마시는 방법"),
-        InfoItemModel("와인 마시는 순서"),
-        InfoItemModel("남은 와인 보관하기")
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,15 +27,9 @@ class InfoFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_info, container, false)
 
-        initRecyclerViewSetting()
-
         return binding.root
     }
 
-    private fun initRecyclerViewSetting() {
-        val rvAdapter = InfoRvAdapter(titleList)
-        infoRV.adapter = rvAdapter
-        infoRV.layoutManager = LinearLayoutManager(context)
-    }
+
 
 }
