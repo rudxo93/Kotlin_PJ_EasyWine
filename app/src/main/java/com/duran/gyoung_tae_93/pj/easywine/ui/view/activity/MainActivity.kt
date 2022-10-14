b567e23b8490655019269241679a5e4380eca7b1
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-10-14 15:25:52 +0900

    Main -> Custom Bottom Navigation / icon 클릭에 따른 fragment 전환 구현

--- app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/activity/MainActivity.kt
+++ app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/activity/MainActivity.kt
@@ -2,11 +2,33 @@ package com.duran.gyoung_tae_93.pj.easywine.ui.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.duran.gyoung_tae_93.pj.easywine.R
+import com.duran.gyoung_tae_93.pj.easywine.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    private val bottomNav by lazy { binding.bottomNavigation }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        bottomNav.itemIconTintList = null // custom한 icon이 나오도록
+
+        setupJetpackNavigation()
+    }
+
+    private fun setupJetpackNavigation() {
+        val host = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+        navController = host.navController
+        bottomNav.setupWithNavController(navController)
     }
 }
