5e94716bb363211ec891b1c00bb1ca470f90fe7f
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-10-14 16:38:10 +0900

    Main -> 고정으로 보여줄 toolbar 구현 / 상단 icon 클릭 시 mypage Activity 이동

--- app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/activity/MainActivity.kt
+++ app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/activity/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.duran.gyoung_tae_93.pj.easywine.ui.view.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -15,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
 
     private val bottomNav by lazy { binding.bottomNavigation }
+    private val topToolbar by lazy { binding.topToolbar }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +27,7 @@ class MainActivity : AppCompatActivity() {
         bottomNav.itemIconTintList = null // custom한 icon이 나오도록
 
         setupJetpackNavigation()
+        setSupportActionBar(topToolbar)
     }
 
     private fun setupJetpackNavigation() {
@@ -30,5 +35,23 @@ class MainActivity : AppCompatActivity() {
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
         navController = host.navController
         bottomNav.setupWithNavController(navController)
+    }
+
+    /**
+     *  Toolbar 상단 Mypage 아이콘 클릭 시 MyPage Actiivty 이동 구현
+     */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.activity_myPage -> {
+                startActivity(Intent(this, MypageActivity::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/activity/MypageActivity.kt
@@ -0,0 +1,12 @@
+package com.duran.gyoung_tae_93.pj.easywine.ui.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.duran.gyoung_tae_93.pj.easywine.R
+
+class MypageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mypage)
+    }
+}
