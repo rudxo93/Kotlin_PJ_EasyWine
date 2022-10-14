59c7fbf8e2c19e6b274de40b3e69274669cfe2fb
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-10-15 00:55:08 +0900

    Info -> "와인 글라스 선택하기" 클릭 시 WineGlassFragment 이동 구현 Error -> WineGlassFragment 에서 뒤로가기 버튼 클릭 후 InfoFragment 로 돌아왔을 때 다시 클릭이 되지 않음(..ing)

--- app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/fragment/InfoFragment.kt
+++ app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/fragment/InfoFragment.kt
@@ -1,12 +1,14 @@
 package com.duran.gyoung_tae_93.pj.easywine.ui.view.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavDirections
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.duran.gyoung_tae_93.pj.easywine.R
@@ -17,6 +19,18 @@ class InfoFragment : Fragment() {
 
     private lateinit var binding: FragmentInfoBinding
 
+    private val moveWineGlass by lazy { binding.linearTitle1 }
+    private val moveWineOpen by lazy { binding.linearTitle2 }
+    private val moveChampagneOpen by lazy { binding.linearTitle3 }
+    private val moveNoOpener by lazy { binding.linearTitle4 }
+    private val moveBrokenCork by lazy { binding.linearTitle5 }
+    private val moveWineChilling by lazy { binding.linearTitle6 }
+    private val moveWineDrink by lazy { binding.linearTitle7 }
+    private val moveWineOrder by lazy { binding.linearTitle8 }
+    private val moveWineKeep by lazy { binding.linearTitle9 }
+
+    private val TAG = InfoFragment::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -26,10 +40,24 @@ class InfoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_info, container, false)
-
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        moveWineGlass.setOnClickListener {
+            Log.d(TAG, "와인 글라스 선택하기 클릭 -> 와인 글라스 선택하기 Fragment로 이동합니다.")
+            it.findNavController().navigate(R.id.action_fragment_info_to_wineGlassFragment)
+        }
 
+        /*initMoveWineGlass()*/
+    }
+
+    private fun initMoveWineGlass() {
+        moveWineGlass.setOnClickListener {
+            Log.d(TAG, "와인 글라스 선택하기 클릭 -> 와인 글라스 선택하기 Fragment로 이동합니다.")
+            view?.findNavController()?.navigate(R.id.action_fragment_info_to_wineGlassFragment)
+        }
+    }
 
 }
