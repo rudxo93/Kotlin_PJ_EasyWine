6bba749d6376688d7e4230bde59de5919c5dc817
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-10-19 14:37:03 +0900

    Info -> 각 정보 layout 클릭 시 각각의 해당하는 activity 이동 구현(Fragment -> Activity 변경)

---
+++ app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/activity/information/NoOpenerActivity.kt
@@ -0,0 +1,12 @@
+package com.duran.gyoung_tae_93.pj.easywine.ui.view.activity.information
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.duran.gyoung_tae_93.pj.easywine.R
+
+class NoOpenerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_no_opener)
+    }
+}
--- app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/fragment/InfoFragment.kt
+++ app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/fragment/InfoFragment.kt
@@ -1,33 +1,29 @@
 package com.duran.gyoung_tae_93.pj.easywine.ui.view.fragment
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.NavDirections
-import androidx.navigation.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.fragment.app.Fragment
 import com.duran.gyoung_tae_93.pj.easywine.R
 import com.duran.gyoung_tae_93.pj.easywine.databinding.FragmentInfoBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.duran.gyoung_tae_93.pj.easywine.ui.view.activity.information.*
 
-class InfoFragment : Fragment() {
+class InfoFragment : Fragment(), View.OnClickListener {
 
     private lateinit var binding: FragmentInfoBinding
 
-    private val moveWineGlass by lazy { binding.linearTitle1 }
-    private val moveWineOpen by lazy { binding.linearTitle2 }
-    private val moveChampagneOpen by lazy { binding.linearTitle3 }
-    private val moveNoOpener by lazy { binding.linearTitle4 }
-    private val moveBrokenCork by lazy { binding.linearTitle5 }
-    private val moveWineChilling by lazy { binding.linearTitle6 }
-    private val moveWineDrink by lazy { binding.linearTitle7 }
-    private val moveWineOrder by lazy { binding.linearTitle8 }
-    private val moveWineKeep by lazy { binding.linearTitle9 }
+    private val wineGlass by lazy { binding.linearTitle1 }
+    private val wineOpen by lazy { binding.linearTitle2 }
+    private val champagneOpen by lazy { binding.linearTitle3 }
+    private val noOpener by lazy { binding.linearTitle4 }
+    private val brokenCork by lazy { binding.linearTitle5 }
+    private val wineChilling by lazy { binding.linearTitle6 }
+    private val wineDrink by lazy { binding.linearTitle7 }
+    private val wineOrder by lazy { binding.linearTitle8 }
+    private val wineKeep by lazy { binding.linearTitle9 }
 
     private val TAG = InfoFragment::class.java.simpleName
 
@@ -45,18 +41,60 @@ class InfoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        moveWineGlass.setOnClickListener {
-            Log.d(TAG, "와인 글라스 선택하기 클릭 -> 와인 글라스 선택하기 Fragment로 이동합니다.")
-            it.findNavController().navigate(R.id.action_fragment_info_to_wineGlassFragment)
-        }
 
-        /*initMoveWineGlass()*/
+        wineGlass.setOnClickListener(this)
+        wineOpen.setOnClickListener(this)
+        champagneOpen.setOnClickListener(this)
+        noOpener.setOnClickListener(this)
+        brokenCork.setOnClickListener(this)
+        wineChilling.setOnClickListener(this)
+        wineDrink.setOnClickListener(this)
+        wineOrder.setOnClickListener(this)
+        wineKeep.setOnClickListener(this)
+
     }
 
-    private fun initMoveWineGlass() {
-        moveWineGlass.setOnClickListener {
-            Log.d(TAG, "와인 글라스 선택하기 클릭 -> 와인 글라스 선택하기 Fragment로 이동합니다.")
-            view?.findNavController()?.navigate(R.id.action_fragment_info_to_wineGlassFragment)
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.linear_title_1 -> {
+                    val intent = Intent(context, WineGlassActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.linear_title_2 -> {
+                    val intent = Intent(context, WineOpenActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.linear_title_3 -> {
+                    val intent = Intent(context, ChampagneOpenActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.linear_title_4 -> {
+                    val intent = Intent(context, NoOpenerActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.linear_title_5 -> {
+                    val intent = Intent(context, BrokenCorkActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.linear_title_6 -> {
+                    val intent = Intent(context, WineChillingActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.linear_title_7 -> {
+                    val intent = Intent(context, WineDrinkActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.linear_title_8 -> {
+                    val intent = Intent(context, WineOrderActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.linear_title_9 -> {
+                    val intent = Intent(context, WineKeepActivity::class.java)
+                    startActivity(intent)
+                }
+
+            }
         }
     }
 
