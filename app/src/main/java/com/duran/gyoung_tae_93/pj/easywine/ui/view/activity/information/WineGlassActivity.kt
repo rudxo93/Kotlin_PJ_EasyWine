194bd3f20891e434e2c6d975dfd95facbaac0737
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-10-20 17:44:20 +0900

    Info -> 와인 글라스 선택하기 탭 구현

--- app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/activity/information/WineGlassActivity.kt
+++ app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/activity/information/WineGlassActivity.kt
@@ -2,11 +2,25 @@ package com.duran.gyoung_tae_93.pj.easywine.ui.view.activity.information
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.duran.gyoung_tae_93.pj.easywine.R
+import com.duran.gyoung_tae_93.pj.easywine.databinding.ActivityWineGlassBinding
 
 class WineGlassActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityWineGlassBinding
+
+    private val topToolbar by lazy { binding.wgToolbar }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_wine_glass)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_wine_glass)
+
+        getToolbarSetting()
+    }
+
+    private fun getToolbarSetting() {
+        topToolbar.setTitle("와인 글라스 선택하기")
+        supportActionBar?.setDisplayShowTitleEnabled(true)
     }
 }
