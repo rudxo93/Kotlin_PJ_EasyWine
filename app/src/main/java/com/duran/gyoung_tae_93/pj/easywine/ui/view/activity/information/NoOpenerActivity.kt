8b77cfb6ad2253ab16e66dad74d6108bc2772e78
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-10-21 23:32:13 +0900

    Info -> 오프너가 없을때 탭 구현

--- app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/activity/information/NoOpenerActivity.kt
+++ app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/activity/information/NoOpenerActivity.kt
@@ -2,11 +2,24 @@ package com.duran.gyoung_tae_93.pj.easywine.ui.view.activity.information
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.duran.gyoung_tae_93.pj.easywine.R
+import com.duran.gyoung_tae_93.pj.easywine.databinding.ActivityNoOpenerBinding
 
 class NoOpenerActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityNoOpenerBinding
+
+    private val topToolbar by lazy { binding.noToolbar }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_no_opener)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_no_opener)
+        getToolbarSetting()
+    }
+
+    private fun getToolbarSetting() {
+        topToolbar.setTitle("오프너가 없을 때")
+        supportActionBar?.setDisplayShowTitleEnabled(true)
     }
 }
