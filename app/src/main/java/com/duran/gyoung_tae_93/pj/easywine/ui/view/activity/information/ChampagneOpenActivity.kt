71480a59803e651301b00b5106a41f86a77b8f79
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-10-21 19:01:23 +0900

    Info -> 샴페인 오픈하는 법 탭 구현

--- app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/activity/information/ChampagneOpenActivity.kt
+++ app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/activity/information/ChampagneOpenActivity.kt
@@ -2,11 +2,24 @@ package com.duran.gyoung_tae_93.pj.easywine.ui.view.activity.information
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.duran.gyoung_tae_93.pj.easywine.R
+import com.duran.gyoung_tae_93.pj.easywine.databinding.ActivityChampagneOpenBinding
 
 class ChampagneOpenActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityChampagneOpenBinding
+
+    private val topToolbar by lazy { binding.coToolbar }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_champagne_open)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_champagne_open)
+        getToolbarSetting()
+    }
+
+    private fun getToolbarSetting() {
+        topToolbar.setTitle("샴페인 오픈하는 법")
+        supportActionBar?.setDisplayShowTitleEnabled(true)
     }
 }
