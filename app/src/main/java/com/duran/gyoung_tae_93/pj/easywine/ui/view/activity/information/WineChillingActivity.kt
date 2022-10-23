a93c2f4a65d1dab8610f683a1068835ad65bfa11
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-10-23 14:27:32 +0900

    Info -> 와인 빠르게 칠링하는 법 탭 구현

--- app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/activity/information/WineChillingActivity.kt
+++ app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/activity/information/WineChillingActivity.kt
@@ -2,11 +2,24 @@ package com.duran.gyoung_tae_93.pj.easywine.ui.view.activity.information
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.duran.gyoung_tae_93.pj.easywine.R
+import com.duran.gyoung_tae_93.pj.easywine.databinding.ActivityWineChillingBinding
 
 class WineChillingActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityWineChillingBinding
+
+    private val topToolbar by lazy { binding.wcToolbar }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_wine_chilling)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_wine_chilling)
+        getToolbarSetting()
+    }
+
+    private fun getToolbarSetting() {
+        topToolbar.setTitle("와인 빠르게 칠링하는 법")
+        supportActionBar?.setDisplayShowTitleEnabled(true)
     }
 }
