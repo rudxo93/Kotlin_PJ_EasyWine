e02e5eec6512891ef775917b1c04ee0be67e0668
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-10-23 21:43:09 +0900

    Info -> 와인 즐기는 방법 탭 구현

--- app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/activity/information/WineDrinkActivity.kt
+++ app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/activity/information/WineDrinkActivity.kt
@@ -2,11 +2,24 @@ package com.duran.gyoung_tae_93.pj.easywine.ui.view.activity.information
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.duran.gyoung_tae_93.pj.easywine.R
+import com.duran.gyoung_tae_93.pj.easywine.databinding.ActivityWineDrinkBinding
 
 class WineDrinkActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityWineDrinkBinding
+
+    private val topToolbar by lazy { binding.wdToolbar }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_wine_drink)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_wine_drink)
+        getToolbarSetting()
+    }
+
+    private fun getToolbarSetting() {
+        topToolbar.setTitle("와인 맛있게 마시는 방법")
+        supportActionBar?.setDisplayShowTitleEnabled(true)
     }
 }
