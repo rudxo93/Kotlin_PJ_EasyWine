e173aad2a3073298aca8fe425a629c2901aaf1e7
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-10-28 02:02:26 +0900

    Note -> DatePickerDialog 기능 구현(구입시기, 시음일자)

--- app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/activity/EditNoteActivity.kt
+++ app/src/main/java/com/duran/gyoung_tae_93/pj/easywine/ui/view/activity/EditNoteActivity.kt
@@ -1,5 +1,6 @@
 package com.duran.gyoung_tae_93.pj.easywine.ui.view.activity
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,16 +10,22 @@ import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.duran.gyoung_tae_93.pj.easywine.R
 import com.duran.gyoung_tae_93.pj.easywine.databinding.ActivityEditNoteBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class EditNoteActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityEditNoteBinding
+
+    private val buyDate by lazy { binding.tvNoteBuyDate }
+    private val drinkDate by lazy { binding.tvNoteDrinkDate }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_note)
 
         getToolbarSetting()
+        getDatePickerDialog()
     }
 
     /**
@@ -42,5 +49,32 @@ class EditNoteActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
+    /**
+     * 구입시기와 시음일자 DatePickerDialog 기능 구현
+     */
+    private fun getDatePickerDialog() {
+        val btnBuyDate = binding.ivNoteBuyDateIcon
+        val btnDrinkDate = binding.ivNoteDrinkDateIcon
 
+        val myCalendar = Calendar.getInstance()
+
+        btnBuyDate.setOnClickListener {
+            val buyDatePicker = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                val dateString = "${year}/${month+1}/${dayOfMonth}"
+                buyDate.setText(dateString)
+            }
+            DatePickerDialog(this, buyDatePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
+        btnDrinkDate.setOnClickListener {
+            val drinkDatePicker = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                val dateString = "${year}/${month+1}/${dayOfMonth}"
+                drinkDate.setText(dateString)
+            }
+            DatePickerDialog(this, drinkDatePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
+    }
 }
